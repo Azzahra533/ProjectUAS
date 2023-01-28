4df0261c27bde7a523ffb08eb9dd91df2364019e
andy_program <116817892+AndySolihin@users.noreply.github.com>
2023-01-28 13:57:33 +0700

    Update uts-layout.blade.php

--- resources/views/layouts/uts-layout.blade.php
+++ resources/views/layouts/uts-layout.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     <title>{{ config('app.name', 'Laravel') }}</title>
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -56,7 +58,7 @@
     </header>
     <!-- Header End -->
 
-    <!-- Hero Start -->
+    <!-- halaman Start -->
     <section class="lg:w-full lg:h-[630vh] sm:w-full sm:h-[530vh]">
         <div class="text-center py-10">
             <h2 class="font-bold">Brands that Trust us</h2>
@@ -190,10 +192,9 @@
             </div>
         </div>
     </section>
+    <!-- halaman End -->
 
-    <!-- Galeri End -->
-
-    <!-- Footer Start -->
+    <!-- Footer -->
     <footer class="w-full h-[100vh] bg-[#05080F]">
         <div class="flex py-5">
             <div class="flex gap-2">
@@ -247,5 +248,11 @@
             <p class=" lg:absolute sm:absolute text-white lg:mt-32 right-5">© 2022 Mangcoding | Powered by mangcoding</p>
         </div>
     </footer>
+    <script>
+    function Menu(e){
+      let list = document.querySelector('ul');
+      e.name === 'menu' ? (e.name = "close",list.classList.add('top-[80px]') , list.classList.add('opacity-100')) :( e.name = "menu" ,list.classList.remove('top-[80px]'),list.classList.remove('opacity-100'))
+    }
+  </script>
   </body>
 </html>
